78a1094c2abc23cea4c62d6d6a2db6852d379d1c
Rishala Itshnyk <rishala.itshnyk@gmail.com>
2023-12-08 11:44:46 +0200

    initial commit

---
+++ Laboratorna_6_4_ITER.Tests/Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Laboratorna_6_4_ITER.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestFindMaxIndex()
+        {
+            int[] array = { 3, 2, 5, 0, 6, 9, 7, 0, 5, 4 };
+            int maxIndex = Program.FindMaxIndex(array, array.Length);
+            Assert.AreEqual(5, maxIndex);
+        }
+
+        [Test]
+        public void TestCalculateProductBetweenZeros()
+        {
+            int[] array = { 3, 2, 5, 0, 6, 9, 7, 0, 5, 4 };
+            int product = Program.CalculateProductBetweenZeros(array, array.Length);
+            Assert.AreEqual(378, product);
+        }
+
+        [Test]
+        public void TestTransformArray()
+        {
+            int[] array = { 3, 2, 5, 0, 6, 9, 7, 0, 5, 4 };
+            Program.TransformArray(array, array.Length);
+
+            int[] expectedArray = { 2, 0, 9, 0, 4, 3, 5, 6, 7, 5 };
+            CollectionAssert.AreEqual(expectedArray, array);
+        }
+    }
+}
---
+++ Laboratorna_6_4_ITER/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Laboratorna_6_4_ITER
+{
+    public class Program
+    {
+        static void FillArray(int[] arr, int n)
+        {
+            Console.WriteLine($"Enter {n} elements for the array:");
+            for (int i = 0; i < n; ++i)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void PrintArray(int[] arr, int n)
+        {
+            Console.Write("Array: ");
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static int FindMaxIndex(int[] arr, int n)
+        {
+            int maxIndex = 0;
+
+            for (int i = 1; i < n; ++i)
+            {
+                if (arr[i] > arr[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            return maxIndex;
+        }
+
+        public static int CalculateProductBetweenZeros(int[] arr, int n)
+        {
+            int firstZeroIndex = -1;
+            int secondZeroIndex = -1;
+
+            for (int i = 0; i < n; ++i)
+            {
+                if (arr[i] == 0)
+                {
+                    if (firstZeroIndex == -1)
+                    {
+                        firstZeroIndex = i;
+                    }
+                    else
+                    {
+                        secondZeroIndex = i;
+                        break;
+                    }
+                }
+            }
+
+            if (firstZeroIndex == -1 || secondZeroIndex == -1)
+            {
+                return -1;
+            }
+
+            int product = 1;
+            for (int i = firstZeroIndex + 1; i < secondZeroIndex; ++i)
+            {
+                product *= arr[i];
+            }
+
+            return product;
+        }
+
+        public static void TransformArray(int[] arr, int n)
+        {
+            int[] tempArray = new int[n];
+            int tempIndex = 0;
+
+            for (int i = 1; i < n; i += 2)
+            {
+                tempArray[tempIndex++] = arr[i];
+            }
+
+            for (int i = 0; i < n; i += 2)
+            {
+                tempArray[tempIndex++] = arr[i];
+            }
+
+            Array.Copy(tempArray, arr, n);
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter the size of the array: ");
+            if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+            {
+                Console.Error.WriteLine("Invalid array size");
+                return;
+            }
+
+            int[] arr = new int[n];
+
+            FillArray(arr, n);
+            PrintArray(arr, n);
+
+            int maxIndex = FindMaxIndex(arr, n);
+            Console.WriteLine($"Index of the maximum element: {maxIndex}");
+
+            int productBetweenZeros = CalculateProductBetweenZeros(arr, n);
+            if (productBetweenZeros == -1)
+            {
+                Console.WriteLine("Error: Two zeros are not found in the array.");
+            }
+            else
+            {
+                Console.WriteLine($"Product between elements with two zeros: {productBetweenZeros}");
+            }
+
+            TransformArray(arr, n);
+            Console.WriteLine("Transformed Array:");
+            PrintArray(arr, n);
+        }
+    }
+}
